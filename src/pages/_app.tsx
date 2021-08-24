d81e664f0836a70dda65c2b547e2e7fe8b2f5060
Rfontt <ritafontenele@ext.cvccorp.com.br>
2021-08-24 18:24:43 -0300

    Adicionando PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta name="description" content="A simple project" />
       </Head>
       <GlobalStyles />
